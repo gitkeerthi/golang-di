84494b809d6ad79a1497970b31ab46348ac124dd
Keerthi Panneerselvam <keerthi.selvam@gmail.com>
2022-04-12 18:55:32 +0530

    Code comments

--- user/user.go
+++ user/user.go
@@ -31,12 +31,14 @@ func (s *UserService) GetUser(id int) (User, error) {
 	return s.userDao.Get(id)
 }
 
+// Factory function to create the real dao
 func NewUserDao(ds *db.DataSource) *PersistingUserDao {
 	return &PersistingUserDao{
 		dataSource: ds,
 	}
 }
 
+// This is our real Dao
 type PersistingUserDao struct {
 	dataSource *db.DataSource
 }
@@ -53,12 +55,7 @@ func (dao *PersistingUserDao) Get(id int) (User, error) {
 	return User{}, nil
 }
 
-func NewMockUserDao() *InMemoryUserDao {
-	return &InMemoryUserDao{
-		dataSource: nil,
-	}
-}
-
+// This is our mock dao. Can be used for unit testing the service
 type InMemoryUserDao struct {
 	dataSource *db.DataSource
 }
@@ -71,4 +68,11 @@ func (dao *InMemoryUserDao) Insert(user User) error {
 func (dao *InMemoryUserDao) Get(id int) (User, error) {
 	// emulate db operations in memory
 	return User{Id: 10, Name: "John Doe"}, nil
+}
+
+// Factory function to create the mock dao
+func NewMockUserDao() *InMemoryUserDao {
+	return &InMemoryUserDao{
+		dataSource: nil,
+	}
 }
